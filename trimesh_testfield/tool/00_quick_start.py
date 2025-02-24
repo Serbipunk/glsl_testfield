3982f712c2ee26a19299767a682cdfd266ac4209
Serbipunk <milesbai6022@gmail.com>
2025-02-25 03:10:19 +1300

    chore: render save error example

--- trimesh_testfield/tool/00_quick_start.py
+++ trimesh_testfield/tool/00_quick_start.py
@@ -10,28 +10,31 @@ trimesh.util.attach_to_log()
 
 class MeshProperties(Enum):
     MESH_DIRECT_CONSTRUCT = 1
+    MESH_CONSTRUCT_AND_PROCESS = 2
+    MESH_LOAD_GLB = 3
+    MESH_LOAD_STL = 4
 
-mesh_properties = MeshProperties.MESH_DIRECT_CONSTRUCT
+mesh_properties = MeshProperties.MESH_LOAD_STL
 
 if mesh_properties == MeshProperties.MESH_DIRECT_CONSTRUCT:
     # mesh objects can be created from existing faces and vertex data
     mesh = trimesh.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0]],
                            faces=[[0, 1, 2]])
-else:
+elif mesh_properties == MeshProperties.MESH_CONSTRUCT_AND_PROCESS:
     # by default, Trimesh will do a light processing, which will
     # remove any NaN values and merge vertices that share position
     # if you want to not do this on load, you can pass `process=False`
     mesh = trimesh.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0]],
                            faces=[[0, 1, 2]],
                            process=False)
-
+elif mesh_properties == MeshProperties.MESH_LOAD_GLB:
     # some formats represent multiple meshes with multiple instances
     # the loader tries to return the datatype which makes the most sense
     # which will for scene-like files will return a `trimesh.Scene` object.
     # if you *always* want a straight `trimesh.Trimesh` you can ask the
     # loader to "force" the result into a mesh through concatenation
     mesh = trimesh.load('/Users/milesbai/lib/trimesh/models/CesiumMilkTruck.glb', force='mesh')
-
+elif mesh_properties == MeshProperties.MESH_LOAD_STL:
     # mesh objects can be loaded from a file name or from a buffer
     # you can pass any of the kwargs for the `Trimesh` constructor
     # to `trimesh.load`, including `process=False` if you would like
